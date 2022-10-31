00f35fa6de2fb48414609f9c22df4a1b53cf058f
Michael Bestas <mkbestas@lineageos.org>
2022-10-31 20:39:59 +0200

    [DNM] virt-common: Set TARGET_KERNEL_CLANG_VERSION to r416183b
    Change-Id: Ic1eb6f7e3cb54bdb67da2fb963426a240f2edda3

--- BoardConfigVirtCommon.mk
+++ BoardConfigVirtCommon.mk
@@ -39,6 +39,8 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.selinux=permissive \
     androidboot.verifiedbootstate=orange
 
+TARGET_KERNEL_CLANG_VERSION := r416183b
+TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_LLVM_BINUTILS := false
 
 # Partitions
